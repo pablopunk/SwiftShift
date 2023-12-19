e6f175e58184565345ef2b418dfc96f2cdf6686c
Pablo P Varela <pablo@pablopunk.com>
2023-12-19 16:29:23 +0100

    Initial Commit

---
+++ Swift Shift/Swift_ShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ShiftApp.swift
+//  Swift Shift
+//
+//  Created by Pablo Pérez Varela on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
