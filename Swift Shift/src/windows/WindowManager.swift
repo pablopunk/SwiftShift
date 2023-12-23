3e20facb2eec4f223fc0210560aaf28fc2fb4c3d
Pablo P Varela <pablo@pablopunk.com>
2023-12-23 03:06:37 +0100

    move window under the cursor, even if not focused

--- Swift Shift/src/windows/WindowManager.swift
+++ Swift Shift/src/windows/WindowManager.swift
@@ -9,26 +9,39 @@ class WindowManager {
         AXUIElementSetAttributeValue(window, kAXPositionAttribute as CFString, pointValue)
     }
     
-    // Function to get the current active window
+    // Function to get the window under the cursor (even if it's not focused)
     static func getCurrentWindow() -> AXUIElement? {
+        // Get the current mouse location
+        let mouseLocation = NSEvent.mouseLocation
+        
         // Create a system-wide accessibility object
         let systemWideElement = AXUIElementCreateSystemWide()
-
-        // Create a variable to hold the focused application
-        var focusedApp: AnyObject?
-
-        // Get the focused application
-        let error: AXError = AXUIElementCopyAttributeValue(systemWideElement, kAXFocusedApplicationAttribute as CFString, &focusedApp)
-
-        if error == .success {
-            // Create a variable to hold the focused window
-            var focusedWindow: AnyObject?
-
-            // Get the focused window
-            AXUIElementCopyAttributeValue(focusedApp as! AXUIElement, kAXFocusedWindowAttribute as CFString, &focusedWindow)
-
-            if let focusedWindow = focusedWindow {
-                return focusedWindow as! AXUIElement
+        
+        // Convert the mouse location to a CGPoint for the hit test
+        let point = CGPoint(x: mouseLocation.x, y: NSScreen.main!.frame.height - mouseLocation.y)
+        
+        // Perform a hit test to find the element under the mouse
+        var element: AXUIElement?
+        let error = AXUIElementCopyElementAtPosition(systemWideElement, Float(point.x), Float(point.y), &element)
+        
+        if error == .success, let element = element {
+            // Check if the found element is a window, or find the window that contains it
+            return getWindowFromElement(element as! AXUIElement)
+        }
+        return nil
+    }
+    
+    // Helper function to find the window containing a given accessibility element
+    private static func getWindowFromElement(_ element: AXUIElement) -> AXUIElement? {
+        var role: AnyObject?
+        AXUIElementCopyAttributeValue(element, kAXRoleAttribute as CFString, &role)
+        if role as? String == kAXWindowRole {
+            return element
+        } else {
+            var parent: AnyObject?
+            AXUIElementCopyAttributeValue(element, kAXParentAttribute as CFString, &parent)
+            if let parent = parent {
+                return getWindowFromElement(parent as! AXUIElement)
             }
         }
         return nil
