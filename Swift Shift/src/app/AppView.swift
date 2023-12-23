b2d2c1881f2bad10b36324726fad67a02f6fdbe7
Pablo P Varela <pablo@pablopunk.com>
2023-12-23 12:58:23 +0100

    Replace login logic with LaunchAtLogin

--- Swift Shift/src/app/AppView.swift
+++ Swift Shift/src/app/AppView.swift
@@ -1,19 +1,14 @@
 import SwiftUI
 import ShortcutRecorder
 import ServiceManagement
+import LaunchAtLogin
+
 
 struct AppView: View {
-    @AppStorage("startAtLogin") private var startAtLogin = false
-    
     var body: some View {
         VStack {
             Text("Swift Shift").font(.title)
-            Toggle(isOn: $startAtLogin) {
-                Text("Start at login")
-            }
-            .onChange(of: startAtLogin) { value in
-                setStartAtLogin(value)
-            }
+            LaunchAtLogin.Toggle()
             if PermissionsManager.hasAccessibilityPermission() {
                 VStack {
                     ForEach(Array(ShortcutType.allCases), id: \.self) { type in
@@ -26,34 +21,6 @@ struct AppView: View {
             }
         }
         .padding(24)
-        .onAppear {
-            // Load the initial state from UserDefaults
-            startAtLogin = UserDefaults.standard.bool(forKey: "startAtLogin")
-        }
-    }
-    
-    private func setStartAtLogin(_ enabled: Bool) {
-        // Create an SMAppService instance with your app's bundle identifier
-        let appService = SMAppService()
-        
-        if enabled {
-            // Register the app service to start at login
-            do {
-                try appService.register()
-            } catch {
-                print("Failed to register app service: \(error.localizedDescription)")
-            }
-        } else {
-            // Unregister the app service
-            appService.unregister { error in
-                if let error = error {
-                    print("Failed to unregister app service: \(error.localizedDescription)")
-                }
-            }
-        }
-        
-        // Save the user preference
-        UserDefaults.standard.set(enabled, forKey: "startAtLogin")
     }
 }
 
