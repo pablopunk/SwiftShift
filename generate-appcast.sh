9d5e67956de7fbcfb56cfb2e63566b92620072f1
Pablo P Varela <pablo@pablopunk.com>
2024-01-04 14:13:15 +0100

    zipping didn't work...

--- generate-appcast.sh
+++ generate-appcast.sh
@@ -7,12 +7,12 @@ app_name="Swift Shift"
 app_name_no_space="SwiftShift"
 
 if [[ -z "$export_folder" ]]; then
-    echo "Usage: $0 <folder containing '$app_name.app'>"
+    echo "Usage: $0 <folder containing ZIP with '$app_name.app'>"
     exit 1
 fi
 
 if [[ ! -d "$export_folder/$app_name.app" ]]; then
-    echo "Usage: $0 <folder containing '$app_name.app'>"
+    echo "Usage: $0 <folder containing ZIP with '$app_name.app'>"
     echo
     echo "Error: $export_folder/$app_name.app does not exist"
     exit 1
@@ -20,12 +20,6 @@ fi
 
 # move current appcast.xml to export folder
 cp ./appcast.xml $export_folder
-
-# zip app
-echo "Generating zip file..."
-pushd "$export_folder"
-zip -r "${app_name_no_space}.zip" "$app_name.app" > /dev/null
-popd
 
 # find executable
 generate_appcast=$(find ~/Library/Developer/Xcode/DerivedData/Swift* -type f -name "*generate_appcast")
