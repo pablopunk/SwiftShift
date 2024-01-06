aeddbfe7b2a4f602407aa064e3f7068744103afa
Pablo P Varela <pablo@pablopunk.com>
2024-01-06 15:25:15 +0100

    sign update in script

--- generate-appcast.sh
+++ generate-appcast.sh
@@ -27,12 +27,17 @@ pushd "$export_folder"
 zip -9 -y -r -q "${app_name_no_space}.zip" "$app_name.app"
 popd
 
-# find executable
+# find Sparkle's bin folder
 generate_appcast=$(find ~/Library/Developer/Xcode/DerivedData/Swift* -type f -name "*generate_appcast")
+bin_folder=$(dirname $generate_appcast)
 
 # generate appcast
 echo "Generating appcast.xml..."
 $generate_appcast $export_folder
+
+# sign update
+echo "Signing update..."
+$bin_folder/sign_update "$export_folder/$app_name_no_space.zip"
 
 # modify appcast.xml to point to latest release ZIP in github
 sed -i '' 's/https:\/\/pablopunk.github.io\/SwiftShift\/SwiftShift.zip/https:\/\/github.com\/pablopunk\/SwiftShift\/releases\/latest\/download\/SwiftShift.zip/g' $export_folder/appcast.xml
