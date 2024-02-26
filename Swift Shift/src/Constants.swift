3853776db6dd3f810e5ade540f412b49cada334e
Pablo P Varela <pablo@pablopunk.com>
2024-02-26 19:05:55 +0100

    ignore HazeOver

--- Swift Shift/src/Constants.swift
+++ Swift Shift/src/Constants.swift
@@ -1,3 +1,3 @@
 
-var IGNORE_APP_BUNDLE_ID = ["com.apple.notificationcenterui", "pl.maketheweb.cleanshotx"]
+var IGNORE_APP_BUNDLE_ID = ["com.apple.notificationcenterui", "pl.maketheweb.cleanshotx", "com.pointum.hazeover"]
 var MAIN_WINDOW_WIDTH = 320.0
