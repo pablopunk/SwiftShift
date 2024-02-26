4716bca833e7d3c622508285d4cb60dc315099e6
Pablo P Varela <pablo@pablopunk.com>
2024-02-26 23:40:21 +0100

    add setapp bundle id to ignored apps

--- Swift Shift/src/Constants.swift
+++ Swift Shift/src/Constants.swift
@@ -1,3 +1,9 @@
 
-var IGNORE_APP_BUNDLE_ID = ["com.apple.notificationcenterui", "pl.maketheweb.cleanshotx", "com.pointum.hazeover"]
+var IGNORE_APP_BUNDLE_ID = [
+  "com.apple.notificationcenterui",
+  "pl.maketheweb.cleanshotx",
+  "pl.maketheweb.cleanshotx-setapp",
+  "com.pointum.hazeover",
+  "com.pointum.hazeover-setapp"
+]
 var MAIN_WINDOW_WIDTH = 320.0
