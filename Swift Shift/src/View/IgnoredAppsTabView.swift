1961c7dc829e31f28b1620f238f8e1764fac7ac7
Pablo P Varela <pablo@pablopunk.com>
2025-05-12 18:59:34 +0200

    padding to note

--- Swift Shift/src/View/IgnoredAppsTabView.swift
+++ Swift Shift/src/View/IgnoredAppsTabView.swift
@@ -70,8 +70,7 @@ struct IgnoredAppsTabView: View {
                     .font(.caption)
                     .foregroundColor(.secondary)
             }
-            .padding(.horizontal)
-            .padding(.bottom)
+            .padding()
         }
         .onAppear {
             loadApps()
