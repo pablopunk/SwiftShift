80bf302e242693275f508029fcc82de971980e31
Pablo P Varela <pablo@pablopunk.com>
2026-05-15 19:49:55 +0200

    Expose default ignored apps in settings

--- Swift Shift/src/Manager/Preferences.swift
+++ Swift Shift/src/Manager/Preferences.swift
@@ -24,20 +24,17 @@ class PreferencesManager {
         if let cached = cachedIgnoredApps {
             return Array(cached)
         }
-        var apps = Set(IGNORE_APP_BUNDLE_ID)
-        if let savedApps = UserDefaults.standard.array(forKey: PreferenceKey.ignoredApps.rawValue) as? [String] {
-            apps.formUnion(savedApps)
-        }
+        var apps = Set(SYSTEM_IGNORED_APP_BUNDLE_ID)
+        apps.formUnion(getUserIgnoredApps())
         cachedIgnoredApps = apps
         return Array(apps)
     }
     
     static func getUserIgnoredApps() -> [String] {
-        // Return only user-added apps
         if let savedApps = UserDefaults.standard.array(forKey: PreferenceKey.ignoredApps.rawValue) as? [String] {
             return savedApps
         }
-        return []
+        return DEFAULT_IGNORED_APP_BUNDLE_ID
     }
     
     static func setUserIgnoredApps(_ apps: [String]) {
@@ -46,22 +43,20 @@ class PreferencesManager {
     }
     
     static func addIgnoredApp(_ bundleId: String) {
+        guard !SYSTEM_IGNORED_APP_BUNDLE_ID.contains(bundleId) else { return }
         var currentList = getUserIgnoredApps()
         
-        // Don't add apps that are already in default list
-        if !IGNORE_APP_BUNDLE_ID.contains(bundleId) && !currentList.contains(bundleId) {
+        if !currentList.contains(bundleId) {
             currentList.append(bundleId)
             setUserIgnoredApps(currentList)
         }
     }
     
     static func removeIgnoredApp(_ bundleId: String) {
-        // Only remove from user list if it's not in the default list
-        if !IGNORE_APP_BUNDLE_ID.contains(bundleId) {
-            var currentList = getUserIgnoredApps()
-            currentList.removeAll { $0 == bundleId }
-            setUserIgnoredApps(currentList)
-        }
+        guard !SYSTEM_IGNORED_APP_BUNDLE_ID.contains(bundleId) else { return }
+        var currentList = getUserIgnoredApps()
+        currentList.removeAll { $0 == bundleId }
+        setUserIgnoredApps(currentList)
     }
     
     static func isAppIgnored(_ bundleId: String) -> Bool {
